1fb744f25ccb50a5348f2620ed0cf70048e7c9f6
vignesh5698 <vigneshit13@gmail.com>
2017-09-08 18:01:03 +0530

    test

--- src/io/Writer1.java
+++ src/io/Writer1.java
@@ -1,17 +1,32 @@
 package io;
 
+import java.io.FileReader;
 import java.io.FileWriter;
 
 public class Writer1 {
 	public static void main(String[] args) {
 		try {
 			FileWriter writer = new FileWriter("sample.txt");
+			writer.write("hi buddy");
+			writer.flush();
+			writer.close();
 			
 		} catch (Exception e) {
+			System.out.println("Exception"+e);
 			
 			// TODO: handle exception
 			
 		}
-	}
+		
+try {
+	FileReader fr = new  FileReader("sample.txt");
+ char []a=new char[50];
+ fr.read(a);
+ for(char c:a){
+	 System.out.println(c);
+ }
+} catch (Exception e) {
+	// TODO: handle exception
+}	}
 }
 
